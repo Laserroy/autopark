afee5834194ff3b99cb605d1be1fc9a926b8ba94
laserX <deectro@gmail.com>
2020-06-28 13:34:02 +0200

    refactor views

--- resources/views/autopark/index.blade.php
+++ resources/views/autopark/index.blade.php
@@ -1,18 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    <table>
-        <th>Name</th>
-        <th>Address</th>
+<div class="container-fluid">
+    <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Cars</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
         @foreach($autoparks as $autopark)
-        <tr>
-            <td>
-                <a href="{{ route('autoparks.show', $autopark) }}">{{ $autopark->name }}</a>
-            </td>
-            <td>{{ $autopark->address }}</td>
-        </tr>
+            <tr>
+                <td><a href="{{ route('autoparks.show', $autopark) }}">{{ $autopark->name }}</a></td>
+                <td>
+                    <ul>
+                        @foreach($autopark->cars as $car)
+                        <li>{{$car->number}}|{{$car->driver}}</li>
+                        @endforeach
+                    </ul>
+                </td>
+                <td>@mdo</td>
+            </tr>
         @endforeach
-    </table>
+        </tbody>
+      </table>
 </div>
 @endsection
--- resources/views/autopark/show.blade.php
+++ resources/views/autopark/show.blade.php
@@ -1,26 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    <table>
-        <tr>
-            <td>Name</td>
-            <td>{{ $autopark->name }}</td>
-        </tr>
-        <tr>
-            <td>Address</td>
-            <td>{{ $autopark->address }}</td>
-        </tr>
-        <tr>
-            <td>Hours</td>
-            <td>{{ $autopark->work_hours }}</td>
-        </tr>
-        <tr>
-            <td>Cars</td>
-            @foreach($autopark->cars as $car)
-            <td>|{{ $car->number }}----{{ $car->driver }}|</td>
-            @endforeach
-        </tr>
-    </table>
+<div class="text-center">
+    <h1>{{ $autopark->name }}</h1>
+    <p>{{ $autopark->address }}</p>
+    <p>{{ $autopark->work_hours }}</p>
+    <table class="table w-50 mx-auto">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">Number</th>
+            <th scope="col">Driver</th>
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                @foreach($autopark->cars as $car)
+                <td>{{ $car->number }}</td>
+                <td>{{ $car->driver }}</td>
+                @endforeach
+            </tr>
+        </tbody>
+      </table>
 </div>
 @endsection
